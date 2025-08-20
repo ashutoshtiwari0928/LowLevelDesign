232ad94bdc8bc0b237d78ef42d6fdc264925e1f9
Ashutosh Tiwari <ashutoshtiwari200128@gmail.com>
2025-08-20 14:27:38 +0530

    Resolved error

--- DesignPatterns/ObjectCreationalPatterns/AbstractFactoryDesignPattern.java
+++ DesignPatterns/ObjectCreationalPatterns/AbstractFactoryDesignPattern.java
@@ -1,6 +1,7 @@
+
 public class AbstractFactoryDesignPattern {
     public static void main(String[] args) {
 
-        System.out.println("Hello world");
+        System.out.println("Hello world, how are you");
     }
 }
